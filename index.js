4421f28e21793270514ef5a44ca24d7c8b7a3de7
Madhurirpatil67 <55598061+Madhurirpatil67@users.noreply.github.com>
2020-01-30 22:12:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ let mongoose=require("mongoose");
 let port=4600||process.env.PORT;
 let auth=require("./Authentication/Routes/auth");
 let user=require("./routes/user");
-let file=require("./FileUpload/route/fileupload");
 app.use(express.json());
-app.use("/uploads",express.static("uploads"));
 mongoose
 .connect("mongodb://localhost/STOREDATA",{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log(`connected to db`))
@@ -15,4 +13,4 @@ app.listen(port,()=>console.log(`connected to port`));
 
 app.use("/api",user);
 app.use("/api",auth);
-app.use("/uploadfile",file);
+
